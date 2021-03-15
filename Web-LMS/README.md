8d617cf4dc19cf7236286330cd03ce86683402de
estebangvillar <77155228+estebangvillar@users.noreply.github.com>
2021-03-15 13:07:13 +0100

    Delete Web-LMS directory

--- Web-LMS/README.md
+++
@@ -1,2 +0,0 @@
-# Web-LMS
-Página Web de Esteban García Villar para la asignatura de Lenguaje de marcas y sistemas de gestión de información.
--- Web-LMS/javascript/functions.js
+++
@@ -1,8 +0,0 @@
-function hora()
-{
-    var d = new Date();
-    var h = d.getHours();
-    var m = d.getMinutes();
-    var res = h+":"+m;
-    return res;
-}
